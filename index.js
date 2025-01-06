8e41d6864426bcc2aeeef866f6ca9a694a97a183
SHAHAB UDDIN <shahab.82946@gmail.com>
2025-01-06 09:02:56 +0600

    jwt created

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 3000;
 require('dotenv').config()
 
 // middlewears======
-app.use(cors())
+app.use(cors({
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"]
+}))
 app.use(express.json())
 
 
@@ -24,12 +27,58 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+	const userCollection = client.db('bistroDB').collection('users')
 	const menuCollection = client.db('bistroDB').collection('menu')
 	const reviewCollection = client.db('bistroDB').collection('reviews');
 	const cartCollection = client.db('bistroDB').collection('carts');
 
-	
 
+    app.post('/jwt', async (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+        res.send({token})
+    })
+
+
+
+    // user related APIs=========
+
+    app.get('/users', async(req, res) => {
+        const result = await userCollection.find().toArray()
+        res.send(result)
+    })
+    app.post('/users', async(req, res) => {
+        const user = req.body;
+        const query = {email: user.email}
+        const existingUser = await userCollection.findOne(query)
+        if(existingUser){
+            return res.send({message: "user already exist", insertedId: null}, )
+        }
+        const result = await userCollection.insertOne(user)
+        res.send(result)
+    })
+
+    app.patch("/users/admin/:id", async (req, res) => {
+        const id = req.params.id 
+        const filter = { _id: new ObjectId(id)}
+        const updatedDoc = {
+            $set: {
+                role: 'admin'
+            }
+        }
+        const result = await userCollection.updateOne(filter, updatedDoc)
+        res.send(result)
+    })
+
+    app.delete('/users/:id', async(req, res) => {
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+        const result = await userCollection.deleteOne(query)
+        res.send(result)
+    })
+
+	
+// Menu related Api=============
 	app.get('/menu', async(req,res) => {
 // const menu = req.body
 const result = await menuCollection.find().toArray()
@@ -56,6 +105,13 @@ app.get('/carts', async(req,res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem)
       res.send(result)
+    })
+
+    app.delete('/carts/:id',async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await cartCollection.deleteOne(query);
+        res.send(result)
     })
   } finally {
     // Ensures that the client will close when you finish/error
